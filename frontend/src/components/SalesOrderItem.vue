71cbbab3ee99cf7ad00d4afa9472a8f660ede91b
herbew <hebrew@gmail.com>
2024-02-12 20:31:00 +0700

    add read fetch api

---
+++ frontend/src/components/SalesOrderItem.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <h1>Sales Order Item</h1>
+        <div v-for="order_item in order_items.data" :key="order_item" class="sales_order_item">
+            <h3>{{ order_item.id }} - {{ order_item.order }}</h3>
+        </div>
+    </div>
+</template>
+<script>
+    export default{
+        data(){
+            return {
+                order_items : []
+            }
+        },
+        mounted(){
+            fetch('http://192.168.0.144:8181/admin/api/sales/order/item/list/',
+                {
+                    method: "GET",
+                    headers: {
+                        "Content-type": "application/json;charset=UTF-8",
+                        "Access-Control-Allow-Origin": "*"
+                    }
+                })
+            .then(res => res.json())
+            .then(data => this.order_items = data)
+            .catch(err => console.log(err.message))
+        }
+    }
+</script>
