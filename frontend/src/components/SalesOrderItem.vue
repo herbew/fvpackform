6918bcc7ce18ce4ffac964731041e45be8c02f7a
herbew <hebrew@gmail.com>
2024-02-14 10:37:02 +0700

    add bootstrap5, vue-awesome-paginate, fetch api url, and mounted data by triggred pagination button, and search button

--- frontend/src/components/SalesOrderItem.vue
+++ frontend/src/components/SalesOrderItem.vue
@@ -1,30 +1,148 @@
 <template>
-    <div>
-        <h1>Sales Order Item</h1>
-        <div v-for="order_item in order_items.data" :key="order_item" class="sales_order_item">
-            <h3>{{ order_item.id }} - {{ order_item.order }}</h3>
-        </div>
-    </div>
+  <div class="container">
+      <div class="card">
+          <div class="card-header">
+              <nav class="navbar navbar-light bg-light">
+                  <div class="container-fluid">
+                      <a class="navbar-brand">Navbar</a>
+                      <form class="d-flex">
+                          <input v-model="text_part" class="form-control me-2"  type="search" placeholder="Product Name .." aria-label="Search">
+                          <button class="btn btn-outline-success" @click="submit" type="button">Search</button>
+                      </form>
+                  </div>
+              </nav>
+          </div>
+          <div class="card-body">
+              <table class="table table-striped">
+                  <thead>
+                      <tr>
+                      <th scope="col">#</th>
+                      <th scope="col">Order Name</th>
+                      <th scope="col">Company Customer</th>
+                      <th scope="col">Customer Name</th>
+                      <th scope="col">Order Date</th>
+                      <th scope="col">Delivered Amount</th>
+                      <th scope="col">Total Amount</th>
+                      </tr>
+                  </thead>
+                  <tbody>
+                      <tr v-for="order_item in order_items" :key="order_item">
+                          <th scope="row" style="text-align: right;">{{ order_item.no }}.</th>
+                          <td style="text-align: left;">
+                              {{ order_item.row.order.order_name }}<br>
+                              <small>{{ order_item.row.product }}</small>
+                          </td>
+                          <td style="text-align: left;">
+                              {{ order_item.row.order.customer.company.name }}
+                          </td>
+                          <td style="text-align: left;">
+                              {{ order_item.row.order.customer.name }}
+                          </td>
+                          <td style="text-align: left;">
+                              {{ order_item.row.order.created_at_local }}
+                          </td>
+                      </tr>
+                  </tbody>
+              </table>
+              
+          </div>
+          <div class="card-footer text-muted">
+              <!-- {{ pagination }} -->
+              <div v-if="count === 0"> <h1>Please insert the Product Name! </h1> </div>
+              <div v-else>
+                <paginate
+                  :page-count="pagination.pages"
+                  :page-range="pagination.per_page"
+                  :margin-pages="pagination.pages"
+                  :click-handler="clickCallback"
+                  :prev-text="'Prev'"
+                  :next-text="'Next'"
+                  :container-class="'pagination'"
+                  :page-class="'page-item'"
+                >
+                </paginate>
+              </div>
+          </div>
+      </div>       
+  </div>
 </template>
+
 <script>
-    export default{
-        data(){
-            return {
-                order_items : []
-            }
-        },
-        mounted(){
-            fetch('http://192.168.0.144:8181/admin/api/sales/order/item/list/',
-                {
-                    method: "GET",
-                    headers: {
-                        "Content-type": "application/json;charset=UTF-8",
-                        "Access-Control-Allow-Origin": "*"
-                    }
-                })
-            .then(res => res.json())
-            .then(data => this.order_items = data)
-            .catch(err => console.log(err.message))
-        }
-    }
+  // Pagination
+  import Paginate from 'vuejs-paginate-next';
+  // import { ref } from 'vue'
+
+  const URL_SALES_ORDER_ITEM  = 'http://192.168.0.144:8181/admin/api/sales/order/item/list/';
+  
+  export default {
+      data(){
+          return {
+              pagination : null,
+              total_amount : 0,
+              count : 0,
+              links : null,
+              order_items : []
+          }
+      },
+
+    async mounted(){
+          this.clickCallback();
+    },
+
+    // Paginate
+    components: {
+      paginate: Paginate,
+    },
+    methods: {
+      
+      submit() {
+          this.clickCallback(1);
+      },
+      clickCallback(pageNum){
+          
+          fetch(URL_SALES_ORDER_ITEM+"?page="+pageNum+"&part="+this.text_part,
+              {
+                  method: "GET",
+                  headers: {
+                      "Content-type": "application/json;charset=UTF-8",
+                      "Access-Control-Allow-Origin": "*"
+                  }
+              })
+          .then(res => res.json())
+          .then(data => {
+              this.order_items = data.data;
+              this.total_amount = data.total_amount;
+              this.pagination = {
+                  'page':data.meta.pagination.page,
+                  'pages':data.meta.pagination.pages,
+                  'count':data.meta.pagination.count,
+                  'per_page':data.meta.pagination.last,
+              };
+              this.links = {
+                  'first':data.links.first,
+                  'next':data.links.next,
+                  'prev':data.links.prev,
+                  'last':data.links.last,
+              };
+              this.count = data.meta.pagination.count;
+          })
+          .catch(err => console.log(err.message))
+          console.log(URL_SALES_ORDER_ITEM+"?page="+pageNum+"&part="+this.text_part);
+          console.log(this.order_items)
+          console.log(this.text_part)
+      }
+    },
+    
+  };
 </script>
+
+<style lang="css">
+  /* Adopt bootstrap pagination stylesheet. */
+  @import "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css";
+
+  /* Write your own CSS for pagination */
+  .pagination {
+  }
+  .page-item {
+  }
+</style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,14 @@
-import { createApp } from 'vue'
+// main.js/ts
+import {createApp} from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// import "./assets/main.css"
+import 'bootstrap/dist/css/bootstrap.css'
+import bootstrap from "bootstrap/dist/js/bootstrap.bundle.js"
+import Paginate from "vuejs-paginate-next";
+
+const app = createApp(App)
+app.use(bootstrap)
+app.use(Paginate)
+app.mount('#app')
+
